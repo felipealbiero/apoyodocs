ec1d210d99b9537587cc583fd7eb1e3a4f7a89ba
felpe522 <felipe.albiero@hotmail.com>
2020-07-22 13:38:42 -0300

    css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,58 +11,7 @@
         <!-- <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet"> -->
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #152F53;
-                color: white;
-                font-family: 'Provicali Regular';
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: white;
-                padding: 0 25px;
-                font-size: 20px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+       
     </head>
     <body>
         <div class="flex-center position-ref full-height">
@@ -82,7 +31,7 @@
 
             <div class="content">
                 <div>
-                    <img src={{asset('/img/banner/blueLogo.jpg')}}>>
+                    <img height="305" src={{asset('/img/banner/blueLogo.jpg')}}>
 
                 </div>
 
