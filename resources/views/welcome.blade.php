1e65f36d54da7ab48f601850c26efe496489d016
felpe522 <felipe.albiero@hotmail.com>
2020-07-24 03:10:08 -0300

    homeCorrigida

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,50 @@
         <!-- <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet"> -->
 
         <!-- Styles -->
-       
+        <style>
+            html, body {
+                background-color: #152F53;
+                color: white;
+                font-family: 'Provicali Regular';
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+            .full-height {
+                height: 100vh;
+            }
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+            .position-ref {
+                position: relative;
+            }
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+            .content {
+                text-align: center;
+            }
+            .title {
+                font-size: 84px;
+            }
+            .links > a {
+                color: white;
+                padding: 0 25px;
+                font-size: 20px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
@@ -43,4 +86,3 @@
             </div>
         </div>
     </body>
-</html>
