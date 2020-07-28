dedad8952cc9a2ca67a6f963c2e7fd49314945b4
felpe522 <felipe.albiero@hotmail.com>
2020-07-28 17:05:29 -0300

    primeiroTeste

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,62 +4,13 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- <link href="{{ asset('/css/estilo.css') }}" rel="stylesheet"> -->
+        <link href="{{ asset('/css/estilo.css') }}" rel="stylesheet">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
           integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
           <title>APOYO DOCS</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #152F53;
-                color: white;
-                /* font-family: 'Provicali Regular'; */
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-            .full-height {
-                height: 100vh;
-            }
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-            .position-ref {
-                position: relative;
-            }
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-            .content {
-                text-align: center;
-            }
-            .title {
-                font-size: 84px;
-            }
-            .links > a {
-                color: white;
-                padding: 0 25px;
-                font-size: 20px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
+        <div class="teste" class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
@@ -80,7 +31,7 @@
 
                 </div>
 
-                <div class="links">
+                <div class="teste" class="links">
                     <a href="https://apoyofinanceiro.com.br">Home</a>
                     <a href="http://bit.ly/apoyo-homeequity">Ficha Cadastral</a>
                     <a href="https://apoyofinanceiro.com.br/blog">Blog</a>
